4da6577be7b14c920a2ed76937a7774ef72f216e
Cara Barter <carabarter@gmail.com>
2021-11-22 22:28:25 -0800

    set up dependencies and boilerplate in server index.js

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const { PORT } = process.env;
+
+//add middleware
+app.use(express.json());
+app.use(express.static('public'));
+
+//use routes
+
+//start server
+app.listen(PORT, function() {
+    console.log(`server is running on ${PORT}`);
+});
