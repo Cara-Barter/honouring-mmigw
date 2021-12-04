21e6495b6f5264aa33a0905255a910245ccdd4eb
Cara Barter <carabarter@gmail.com>
2021-12-03 18:37:40 -0800

    added server side auth in the back end

--- server/routes/participants.js
+++ server/routes/participants.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('knex')(require('../knexfile').development);
+const jwt = require('jsonwebtoken');
+
+const authorize = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json({message: 'No token found'})
+  }
+  const authTokenArray = req.headers.authorization.split(' ');
+  if (authTokenArray[0].toLowerCase() !== 'bearer' && authTokenArray.length !== 2) {
+    return res.status(401).json({message: 'Invalid token'});
+  }
+
+  jwt.verify(authTokenArray[1], process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({message: 'The token is expired or invalid'});
+    }
+    req.payload = decoded;
+    next();
+  });
+}
 
 /* Get all participants */
-router.get('/', (req, res) => {
+router.get('/', authorize, (req, res) => {
     knex('participants as p')
     .innerJoin('extra_info as e', 'e.participant_id', 'p.id')
     .then((participantData) => {
-        res.status(200).json(participantData);
+        res.status(200)
+        .json({
+          tokenInfo: req.payload,
+          sensitiveInfo: participantData
+        }
+        );
     })
     .catch(() => {
         res.status(400).json({
@@ -17,7 +41,8 @@ router.get('/', (req, res) => {
 });
 
 //get participants by id
-router.get('/:id', (req, res) => {
+router.get('/:id', authorize, (req, res) => {
+  console.log(req.payload);
     knex.from('participants as p')
         .innerJoin('extra_info as e', 'e.participant_id', 'p.id')
         .where('p.id', req.params.id)
@@ -39,50 +64,49 @@ router.get('/:id', (req, res) => {
 
 //create new participant
 router.post('/', (req, res) => {
-    console.log(req.body);
     
-    const { 
-      firstName, 
-      lastName, 
-      email, 
-      address1, 
-      address2, 
-      city, 
-      province, 
-      postalCode, 
-      country,
-      nation,
-      gender,
-      survivor,
-      age,
-      dedicate,
-      shirtSize,
-      phone,
-      organization
-    } = req.body;
+  const { 
+    firstName, 
+    lastName, 
+    email, 
+    address1, 
+    address2, 
+    city, 
+    province, 
+    postalCode, 
+    country,
+    nation,
+    gender,
+    survivor,
+    age,
+    dedicate,
+    shirtSize,
+    phone,
+    organization
+  } = req.body;
 
-    const participantInfo = {
-      firstName,
-      lastName,
-      email,
-      address1,
-      address2,
-      city,
-      province,
-      postalCode,
-      country,
-      nation,
-      gender,
-      survivor
-    }
+  const participantInfo = {
+    firstName,
+    lastName,
+    email,
+    address1,
+    address2,
+    city,
+    province,
+    postalCode,
+    country,
+    nation,
+    gender,
+    survivor
+  }
 
-    const extraInfo = {
-      age,
-      dedicate,
-      shirtSize,
-      phone,
-      organization
-    }
+  const extraInfo = {
+    age,
+    dedicate,
+    shirtSize,
+    phone,
+    organization
+  }
 
   let participantId;
   
@@ -108,9 +132,7 @@ router.post('/', (req, res) => {
     });
 });
 
-/*
- * Update a participant
- */
+//Update a participant
 router.put('/:id', (req, res) => {
     knex('participants')
       .where({ id: req.params.id })
@@ -127,24 +149,21 @@ router.put('/:id', (req, res) => {
           message: `Error updating participant ${req.params.id}`,
         });
       });
-  });
-  
-  /*
-   * Delete a participant
-   */
-  router.delete('/:id', (req, res) => {
-    knex('participants')
-      .where({ id: req.params.id })
-      .del(req.body)
-      .then((data) => {
-        res.sendStatus(204);
-      })
-      .catch(() => {
-        res.status(400).json({
-          message: `Error deleting participant ${req.params.id}`,
-        });
-      });
-  });
-  
+});
 
+//Delete a participant
+router.delete('/:id', (req, res) => {
+  knex('participants')
+    .where({ id: req.params.id })
+    .del(req.body)
+    .then((data) => {
+      res.sendStatus(204);
+    })
+    .catch(() => {
+      res.status(400).json({
+        message: `Error deleting participant ${req.params.id}`,
+      });
+    });
+});
+  
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,23 +1,74 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-require('dotenv').config();
 const cors = require('cors');
+const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
 const participantsRoutes = require('./routes/participants');
 const honouringRoutes = require('./routes/honouring');
-const { PORT } = process.env || 5000;
+
 
 //add middleware
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
 
+//TODO store users on DB
+const users = [
+    {
+        username: 'Cara',
+        name: 'Cara',
+        password: 'mmiwg'
+    }
+];
+
+// const authorize = (req, res, next) => {
+//     if (!req.headers.authorization) {
+//       return res.status(401).json({message: 'No token found'})
+//     }
+//     const authTokenArray = req.headers.authorization.split(' ');
+//     if (authTokenArray[0].toLowerCase() !== 'bearer' && authTokenArray.length !== 2) {
+//       return res.status(401).json({message: 'Invalid token'});
+//     }
+  
+//     jwt.verify(authTokenArray[1], process.env.JWT_SECRET, (err, decoded) => {
+//       if (err) {
+//         return res.status(401).json({message: 'The token is expired or invalid'});
+//       }
+//       req.payload = decoded;
+//       next();
+//     });
+// }
+
+app.post('/login', (req, res) => {
+    const {username, password} = req.body;
+
+    const foundUser = users.find(user => user.username === username);
+
+    if (!foundUser) {
+    return res.status(401).json({message: "No user found. Please check username." });
+}
+
+if(foundUser.password === password) {
+    // Generate token and send back
+    const token = jwt.sign({
+    name: foundUser.name,
+    username: foundUser.username,
+    loginTime: Date.now()
+    }, process.env.JWT_SECRET, {expiresIn: '59m'});
+    return res.status(200).json({token});
+} else {
+    return res.status(403).json({message: "Invalid username or password" }); 
+}
+});
+
 //use routes
 app.use('/participants', participantsRoutes);
 app.use('/foodburning', honouringRoutes);
 
 //start server
-app.listen(PORT, function() {
-    console.log(`server is running on ${PORT}`);
+app.listen(port, function() {
+    console.log(`server is running on ${port}`);
 });  
   
   
