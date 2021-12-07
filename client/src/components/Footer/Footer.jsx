3f600e973c92997b9edfc750a3466ebd86c61afe
Cara Barter <carabarter@gmail.com>
2021-12-06 23:29:51 -0800

    styled footer for mobile, tablet and desktop. added ttll logo with a link to their website

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,24 +1,36 @@
 import './Footer.scss';
+import logo from '../../assets/logo/ttll-logo.png';
 
 function Footer() {
   return (
     <article className="footer">
-      <h2 className="footer__title">Contact Us</h2>
-      <p className="footer__text">
-        RHSW Program Coordinator
-      </p>
-      <a href="mailto:RHSWCoor@tsowtunlelum.org" className="footer__link">
-        <p className="footer__text">RHSWCoor@tsowtunlelum.org</p>
-      </a>
-      <p className="footer__text">250-390-3123</p>
+      <div className="footer__container">
+        <h2 className="footer__title">Contact Us</h2>
+        <p className="footer__text">
+          RHSW Program Coordinator
+        </p>
+        <a href="mailto:RHSWCoor@tsowtunlelum.org" className="footer__link">
+          <p className="footer__text">RHSWCoor@tsowtunlelum.org</p>
+        </a>
+        <p className="footer__text">250-390-3123</p>
+        <a
+          href="https://tsowtunlelum.org"
+          target="_blank"
+          rel="noreferrer"
+          className="footer__link"
+        >
+          <p className="footer__text">tsowtunlelum.org</p>
+        </a>
+      </div>
       <a
         href="https://tsowtunlelum.org"
         target="_blank"
         rel="noreferrer"
         className="footer__link"
       >
-        <p className="footer__text">tsowtunlelum.org</p>
+        <img src={logo} alt="Tsow Tun Lelum Logo" className="footer__logo" />
       </a>
+      
     </article>
   );
 }
