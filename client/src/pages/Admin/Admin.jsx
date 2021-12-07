e2c80aa9b4f095bd00f973b6479b2c3fbfd6ed19
Cara Barter <carabarter@gmail.com>
2021-12-07 12:38:03 -0800

    tweaked header styling and styled admin nav for mobile, tablet and desktop

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,8 +1,10 @@
+import './Admin.scss';
 import axios from 'axios';
 import { Component } from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 import './Admin.scss';
 import Button from '../../components/Button/Button';
+import logo from '../../assets/logo/MMIWG-logo.png';
 import ParticipantList from '../../components/ParticipantList/ParticipantList';
 import Shirts from '../../components/Shirts/Shirts';
 import SingleParticipant from '../../components/SingleParticipant/SingleParticipant';
@@ -58,26 +60,32 @@ class Admin extends Component {
         
         return(
             <article className="admin">
-                <nav className="admin__nav">
-                    <Link to='/admin/participants' className="admin__link">
-                        <Button 
-                            classname='admin__btn'
-                            text='Walk Participants'
-                        />
-                    </Link> 
-                    <Link to='/admin/shirts' className="admin__link">
-                        <Button 
-                            classname='admin__btn'
-                            text='T-Shirts List'
-                        />
-                    </Link>
-                    <Link to='/admin/foodburning' className="admin__link">
-                        <Button 
-                            classname='admin__btn'
-                            text='MMIWG Food Burning'
-                        />
-                    </Link>      
-                </nav>
+                <section className="admin__wrapper">
+                    <nav className="admin__nav">
+                        <Link to='/admin/participants' className="admin__link">
+                            <Button 
+                                className='admin__btn'
+                                text='Walk Participants'
+                            />
+                        </Link> 
+                        <Link to='/admin/shirts' className="admin__link">
+                            <Button 
+                                className='admin__btn'
+                                text='T-Shirts List'
+                            />
+                        </Link>
+                        <Link to='/admin/foodburning' className="admin__link">
+                            <Button 
+                                className='admin__btn'
+                                text='MMIWG Food Burning'
+                            />
+                        </Link>      
+                    </nav>
+                    <img 
+                        className="admin__logo" 
+                        src={logo} 
+                        alt='art of woman in red dress with arms outstretched' />
+                    </section>
                 <Switch>
                     <Route 
                         path='/admin/participants'
