204bae754ee55f8e851331ca414cf08551319a16
Cara Barter <carabarter@gmail.com>
2021-12-07 18:58:57 -0800

    styled food burning list

--- client/src/components/FoodBurning/FoodBurning.jsx
+++ client/src/components/FoodBurning/FoodBurning.jsx
@@ -6,21 +6,23 @@ function HonoureesList ({ honourees }) {
     
     return (
         <article className="honourees">
-            <h1 className="honourees__title">MMIWG Food Burning List</h1>
-            <ul className="honourees__list">
-                {honourees.sensitiveInfo && honourees.sensitiveInfo
-                    .map((honouree) => {
-                        console.log(honouree);
-                        return (
-                        <li 
-                                className="honourees__item"
-                                key={honouree.id}>
-                                <Honouree honouree={honouree} />
-                        </li> 
-                        );
-                    }
-                )}
-            </ul>
+            <div className="honourees__container">
+                <h1 className="honourees__title">MMIWG Food Burning List</h1>
+                <ul className="honourees__list">
+                    {honourees.sensitiveInfo && honourees.sensitiveInfo
+                        .map((honouree) => {
+                            console.log(honouree);
+                            return (
+                            <li 
+                                    className="honourees__item"
+                                    key={honouree.id}>
+                                    <Honouree honouree={honouree} />
+                            </li> 
+                            );
+                        }
+                    )}
+                </ul>
+            </div>
         </article>
     )
 }
--- client/src/components/Honouree/Honouree.jsx
+++ client/src/components/Honouree/Honouree.jsx
@@ -4,12 +4,38 @@ function Honouree ({honouree}) {
     console.log(honouree);
     return (
         <article className="honouree">
-            <h2 className='honouree__title'>{honouree.yourName}</h2>
-            <p className='honouree__details'>{honouree.lovedOnesName}</p>
-            <p className='honouree__details'>{honouree.nation}</p>
-            <p className='honouree__details'>{honouree.gender}</p>
-            <p className='honouree__details'>{honouree.community}</p>
-            <p className='honouree__details'>{honouree.relationship}</p>
+            <div className="honouree__container">
+                <h2 className='honouree__title'>
+                    <span className='honouree__label'>
+                        Registrant's Name:
+                    </span>{` ${honouree.yourName}`}
+                </h2>
+                <p className='honouree__details'>
+                    <span className='honouree__label'>
+                        MMIWG Name:
+                    </span>{` ${honouree.lovedOnesName}`}
+                </p>
+                <p className='honouree__details'>
+                    <span className='honouree__label'>
+                        Their Nation:
+                    </span>{` ${honouree.nation}`}
+                </p>
+                <p className='honouree__details'>
+                    <span className='honouree__label'>
+                        Their Gender:
+                    </span>{` ${honouree.gender}`}
+                </p>
+                <p className='honouree__details'>
+                    <span className='honouree__label'>
+                        Their Community:
+                    </span>{` ${honouree.community}`}
+                </p>
+                <p className='honouree__details'>
+                    <span className='honouree__label'>
+                        Relationship:
+                    </span>{` ${honouree.relationship}`}
+                </p>
+            </div>
         </article>
     )
 }
