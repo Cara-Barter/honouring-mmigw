119692f4e1867b6465479ee34b7636b47f9d995e
Cara Barter <carabarter@gmail.com>
2021-12-07 22:33:45 -0800

    fixed logo layout and width in header and footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,32 +5,33 @@ function Footer() {
   return (
     <article className="footer">
       <div className="footer__container">
-        <h2 className="footer__title">Contact Us</h2>
-        <p className="footer__text">
-          RHSW Program Coordinator
-        </p>
-        <a href="mailto:RHSWCoor@tsowtunlelum.org" className="footer__link">
-          <p className="footer__text">RHSWCoor@tsowtunlelum.org</p>
-        </a>
-        <p className="footer__text">250-390-3123</p>
+        <div className="footer__wrapper">
+          <h2 className="footer__title">Contact Us</h2>
+          <p className="footer__text">
+            RHSW Program Coordinator
+          </p>
+          <a href="mailto:RHSWCoor@tsowtunlelum.org" className="footer__link">
+            <p className="footer__text">RHSWCoor@tsowtunlelum.org</p>
+          </a>
+          <p className="footer__text">250-390-3123</p>
+          <a
+            href="https://tsowtunlelum.org"
+            target="_blank"
+            rel="noreferrer"
+            className="footer__link"
+          >
+            <p className="footer__text">tsowtunlelum.org</p>
+          </a>
+        </div>
         <a
           href="https://tsowtunlelum.org"
           target="_blank"
           rel="noreferrer"
           className="footer__link"
         >
-          <p className="footer__text">tsowtunlelum.org</p>
+          <img src={logo} alt="Tsow Tun Lelum Logo" className="footer__logo" />
         </a>
-      </div>
-      <a
-        href="https://tsowtunlelum.org"
-        target="_blank"
-        rel="noreferrer"
-        className="footer__link"
-      >
-        <img src={logo} alt="Tsow Tun Lelum Logo" className="footer__logo" />
-      </a>
-      
+      </div> 
     </article>
   );
 }
